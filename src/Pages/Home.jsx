5cb1761286377d054f13027a2dcdf242918761b6
Shashi <shashikumarsharma59@gmail.com>
2023-09-13 13:23:52 +0530

    updated todo  code

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,51 +1,62 @@
-import { Box, Button, FormControl, FormGroup, TextField, Typography } from "@mui/material"
-import React, { useState } from "react"
+import { Box, TextField, Typography } from "@mui/material"
+import React, {useState } from "react"
 import TodoList from "./TodoList"
+import { BsPlusCircleFill } from "react-icons/bs"
+import { FaEdit } from "react-icons/fa"
 
 const Home = () => {
   const [todo, setTodo] = useState("")
   const [todos, setTodos] = useState([])
-
+  const [editId, setEditId] = useState("")
 
   const handleChange = (evt) => {
-    setTodo(evt.target.value)
+    return setTodo(evt.target.value)
   }
 
+  // edit and delete todo list
   const handleAdd = (e) => {
     e.preventDefault()
-    if (todo !== "") {
-      setTodos((list) => {
-        return [...list, todo]
-      })
+    if (editId) {
+      const editTodo = todos.find((i) => i.id === editId)
+      const upDateTodos = todos.map((elem) =>
+        elem.id === editTodo.id
+          ? (elem = { id: elem.id, todo })
+          : { id: elem.id, todo: elem.todo }
+      )
+      setTodos(upDateTodos)
+      setEditId(0)
+      setTodo("")
+      return
     }
-    setTodo("")
+    if (todo !== "") {
+      setTodos([{ id: `${todo}-${Date.now()}`, todo }, ...todos])
+      setTodo("")
+    }
   }
 
-  const handleDelete = (id) => { 
-    setTodos((list) => {
-      return list.filter((ele, index) => {
-        return index !== id
-      })
-    })
+  // delete todo list
+  const handleDelete = (id) => {
+    const delTodo = todos.filter((list) => list.id !== id)
+    setTodos([...delTodo])
   }
 
-  const handleEdit=(id)=>{
-   console.log(id);
-   const editData=todos.filter((ele,index)=> index ===id)
-   setTodo(editData)
+  // edit todo list
+  const handleEdit = (id) => {
+    const editData = todos.find((ele) => ele.id === id)
+    setTodo(editData.todo)
+    setEditId(id)
   }
-
 
   return (
     <Box className=" w-[90%] mdl:w-[50%] sml:w-[60%] lg:w-[50%] xl:w-[40%]  mx-auto  m-4 p-4 rounded-lg shadow-xl  bg-white border border-gray-300 text-center">
-        <Box className="text-center py-3">
-          <Typography variant="h3" component="h3" className="signup_title">
-            Todo List
-          </Typography>
-        </Box>
-      <form className="w-[100%]" onSubmit={(e)=>handleAdd(e)}>
+      <Box className="text-center py-3">
+        <Typography variant="h1" component="h1" className="signup_title">
+          Todo - List
+        </Typography>
+      </Box>
+      <form className="w-[100%]" onSubmit={(e) => handleAdd(e)}>
         <Box className="input_box">
-          <Box className="w-[100%]">
+          <Box className="w-[100%] flex justify-between items-center">
             <TextField
               onChange={handleChange}
               className="input_field"
@@ -54,23 +65,27 @@ const Home = () => {
               autoFocus={true}
               variant="standard"
               placeholder="Enter your todos"
+              autoComplete="off"
             />
-          </Box>
-          <Box>
-            <Button variant="outlined" type="submit" >
-              Add
-            </Button>
+            {editId ? (
+              <FaEdit className=" text-green-400 h-8 w-8" onClick={handleAdd} />
+            ) : (
+              <BsPlusCircleFill
+                className=" text-violet-500 h-8 w-8"
+                onClick={handleAdd}
+              />
+            )}
           </Box>
         </Box>
       </form>
-      <Box>
+      <Box className="mt-4">
         <ul>
-          {todos.map((list, index) => {
+          {todos.map((list) => {
             return (
               <TodoList
-                key={index}
+                key={list.id}
+                id={list.id}
                 list={list}
-                id={index}
                 onSelect={handleDelete}
                 onEdit={handleEdit}
               />
--- src/Pages/TodoList.jsx
+++ src/Pages/TodoList.jsx
@@ -1,15 +1,15 @@
 import React from "react"
 import { MdDeleteSweep } from "react-icons/md"
 import { FaEdit } from "react-icons/fa"
-import { Box, Button } from "@mui/material"
+import { Box, Button, Checkbox } from "@mui/material"
 
 const TodoList = (props) => {
 
-  const { onSelect, id,list,onEdit } = props
+  const { onSelect,id,list,onEdit } = props
 
   return (
-    <Box className="flex items-center justify-between bg-black text-white mt-1 rounded-lg p-2">
-      <li>{list}</li>
+    <Box className="flex items-center justify-between bg-violet-500 text-white  mt-1 rounded-lg p-1">
+      <li className=" font-bold"><Checkbox size="small" className="checkbox"/>{list.todo}</li>
       <Box>
         <Button onClick={() => onSelect(id)}>
           <MdDeleteSweep className="w-8 h-8 text-red-400" />
